1550c500c5b191f1f42039dffd62ed73e5ac520a
PaolaMarquez <paola.marquez@correo.unimet.edu.ve>
2023-03-30 00:57:56 -0400

    Detail no estático

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -15,9 +15,9 @@ export default function ProductCard({ product }) {
       >
         <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
           <img
-            //   src={`${imgUrl}${movie.poster_path}`}
-            // scr={product.image}
-            scr="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80"
+            // src={`${imgUrl}${movie.poster_path}`}
+            src={product.image}
+            // src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80"
             alt={product.name}
             className="h-full w-full object-cover object-center lg:h-full lg:w-full"
           />
